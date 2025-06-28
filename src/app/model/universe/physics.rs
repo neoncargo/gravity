dfcc71ace561a6b1e0745c8533d74b833d4d686e
neoncargo <koalaresearch@proton.me>
2025-06-28 21:39:39 +0700

    add channel multithreading

--- src/app/model/universe/physics.rs
+++ src/app/model/universe/physics.rs
@@ -2,6 +2,7 @@ mod gravity;
 mod collision_logic;
 
 use nannou::geom::Vec2;
+use threadpool::ThreadPool;
 
 use gravity::Gravity;
 use collision_logic::CollisionLogic;
@@ -12,6 +13,7 @@ use super::Body;
 pub struct Physics {
     gravity: Gravity,
     _collision_logic: CollisionLogic,
+    thread_pool: threadpool::ThreadPool,
 }
 
 impl Physics {
@@ -19,6 +21,7 @@ impl Physics {
         Self {
             gravity: Gravity,
             _collision_logic: CollisionLogic::new(),
+            thread_pool: ThreadPool::new(4),
         }
     }
 
@@ -26,13 +29,27 @@ impl Physics {
         let mut delta_vels: Vec<Vec2> = vec![Vec2::ZERO ; bodies.len()];
 
         if CONFIG_MANAGER.is_gravity_enabled() {
+            let (tx, rx) = std::sync::mpsc::channel();
+
             for i in 0..bodies.len() {
                 for j in i+1..bodies.len() {
-                    let (delta1, delta2) = self.gravity.delta_velocities(&bodies[i], &bodies[j], delta_time_sec);
-                    delta_vels[i] += delta1;
-                    delta_vels[j] += delta2;
+                    let tx = tx.clone();
+                    let body1 = bodies[i].clone();
+                    let body2 = bodies[j].clone();
+                    self.thread_pool.execute(move || {
+                        let gravity = Gravity;
+                        tx.send((gravity.delta_velocities(&body1, &body2, delta_time_sec), i, j)).expect("send should be ok");
+                    });
                 }
             }
+            drop(tx);
+
+            for received in rx {
+                delta_vels[received.1] += received.0.0;
+                delta_vels[received.2] += received.0.1;
+            }
+
+            self.thread_pool.join();
         }
 
         for i in 0..bodies.len() {
