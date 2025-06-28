7642765fdb8308d4c3128c44d44da8576d98f89a
neoncargo <koalaresearch@proton.me>
2025-06-28 21:40:02 +0700

    add save, config

---
+++ src/model/save_manager/save.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use log::{debug, warn};
+use serde::{Serialize, Deserialize};
+
+use super::Body;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Save {
+    pub bodies: Vec<Body>,
+}
+
+impl Save {
+    pub fn new() -> Self { Self { bodies: Vec::new() } }
+
+    pub fn from_file(name: &str) -> Save {
+        debug!("Save name: {name}");
+
+        let save_str = fs::read_to_string(name).unwrap_or_else(|e| {
+            warn!("Failed to read save: {}", e);
+            String::new()
+        });
+        debug!("Save str: {save_str}");
+
+        serde_json::from_str(&save_str).unwrap_or_else(|e| {
+            warn!("Failed to deserialize save: {}", e);
+            Save::new()
+        })
+    }
+
+
+}
